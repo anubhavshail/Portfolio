482a869f45a9f620b52f193dd5ef989fadb91b17
Anubhav Shail <anubhavshail12@gmail.com>
2024-05-09 22:52:01 +0530

    Banner page updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,39 +1,86 @@
 import React from 'react';
+import{ useState, useEffect} from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import TrackVisibility from 'react-on-screen';
 import HeroImage from './hero_image';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
 
 const renderBackgroundImage = () => {
-    <div>
+    return (
+    <div className="" style={{ maxHeight: "650px"}}>
         <HeroImage/>
     </div>
+    )
 }
 
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Backend Developer", "MERN Developer"];
+    const [text, setText] = useState('');
+    const [index, setIndex] = useState(1);
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length-1) : fullText.substring(0, text.length+1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta/2);
+        }
+
+        if(!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex -1);
+            setDelta(period);
+        }
+        else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        }
+        else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
     return (
         <section className='banner' id='home'>
             <Container>
-                <Row className='aligh-items-center'>
+                <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7}>
                         <TrackVisibility>
                             <div className='banner__content'>
                                 <span className='tagline'>Welcome to my Portfolio</span>
-                                <h1>Hi! I'm Anubhav Shail</h1>
+                                <h1>{`Hi! I'm Anubhav Shail`} <span className='wrap'>{text}</span></h1>
                                 <p>
                                     Looking to start the career as an entry-level software engineer with a reputed firm driven by technology. Proficient in C++, Python, GIT. Skilled in Web Development. Passionate about implementing and launching new projects.
                                 </p>
+                                <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
                             </div>
                         </TrackVisibility> 
                     </Col>
-                    <Col xs={12} md={6} xl={7}>
+                    <Col xs={12} md={6} xl={5}>
                         <div>
-                            {renderBackgroundImage}
-                        </div>
+                            {renderBackgroundImage()}
+                        </div> 
                     </Col>
                 </Row>
                 
-            </Container>
-            
+            </Container>           
         </section>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,36 @@
 import {Navbar, Nav, Container} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 import logo from '../assets/img/79QtjF-LogoMakr.png';
+import github from '../assets/img/github.svg';
+import linkedin from '../assets/img/linkedin.svg';
+import facebook from '../assets/img/facebook.svg';
 
 function NavBar() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if(window.scrollY > 50) {
+        setScrolled(true);
+      }
+      else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (link) => {
+    setActiveLink(link);
+  }
+
   return (
     <>
-      <Navbar expand="md" className='my-navbar'>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
             <img src={logo} alt="Logo" />
@@ -14,11 +40,18 @@ function NavBar() {
           </Navbar.Toggle>
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className="me-auto">
-              <Nav.Link href="#home" className='nav-link' >Home</Nav.Link>
-              <Nav.Link href="#projects" className='nav-link'>Projects</Nav.Link>
-              <Nav.Link href="#skills" className='nav-link'>Skills</Nav.Link>
-              <Nav.Link href="#contact" className='nav-link'>Contact</Nav.Link>
+              <Nav.Link href="#home" className={activeLink === 'home' ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('home')} >Home</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             </Nav>
+            <span className='navbar-text'>
+              <div className='social-icon'>
+                <a href="#"><img src={github} alt="" /></a>
+                <a href="#"><img src={linkedin} alt="" /></a>
+                <a href="#"><img src={facebook} alt="" /></a>
+              </div>
+              <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+            </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
