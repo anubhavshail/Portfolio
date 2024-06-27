aa6febaf500c74d410ec71408d4a42fa5cce373c
Anubhav Shail <anubhavshail12@gmail.com>
2024-06-27 22:54:48 +0530

    Skills section changed

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,21 +1,15 @@
 import { Col } from 'react-bootstrap';
 
-function ProjectCard({ title, description, imgUrl, gitUrl}) {
+export const ProjectCard = ({ title, description, imgUrl}) => {
   return (
     <Col size={12} sm={6} md={4}>
-        <a href={gitUrl} target='_blank' rel='noopener noreferrer'>
-            <div className='project_img'>
-                <img src={imgUrl} alt='project img' />
-                <div className='proj_txt'>
-                    <h4>{title}</h4>
-                    <span>
-                    {description}
-                    </span>
-                </div>
-            </div>
-        </a>
+      <div className='proj-imgbx'>
+        <img src={imgUrl} alt="" />
+        <div className='proj-txtx'>
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
     </Col>
-  );
+  )
 }
-
-export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,9 @@
-import { Container, Row } from "react-bootstrap"
-import ProjectCard from "./ProjectCard"
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap"
+import { ProjectCard } from "./ProjectCard"
 import img1 from "../assets/img/mail.jpg"
+import colorSharp2 from "../assets/img/color-sharp2.png"
+//import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
     
@@ -23,20 +26,57 @@ export const Projects = () => {
             imgUrl: img1,
             gitUrl: "g3"
         }
-    ]
+    ];
 
     return (
         <section className="project" id="projects">
-            <h2>Projects</h2>
             <Container>
                 <Row>
-                    {projects.map((project, index) => {
-                        return (
-                            <ProjectCard key={index} {...project}/>                        
-                        )
-                    })}
+                    <Col size={12}>
+                        <TrackVisibility>
+                            {({isVisible}) =>
+                            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                <h2>Projects</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quibusdam culpa quos molestiae ad odit nulla placeat amet doloribus est voluptas temporibus quas inventore ratione incidunt, qui, cumque cupiditate? Quisquam.</p>
+                                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                        </Nav.Item>
+                                    </Nav>
+                                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : "" }>
+                                        <Tab.Pane eventKey="first">
+                                            <Row>
+                                                {
+                                                    projects.map((project, index) => {
+                                                        return (
+                                                            <ProjectCard key={index} {...project} />
+                                                        )
+                                                    })
+                                                }
+                                            </Row>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="section">
+                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis debitis quos quisquam voluptates excepturi tempore doloribus mollitia nostrum molestias! Aut repudiandae error sint quam cumque iste dicta vitae impedit perspiciatis!</p>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="third">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas quam necessitatibus et exercitationem assumenda quasi? Ipsum eveniet eos vero quae ipsam dolorum quidem placeat, amet non illum cum officia.</p>
+                                        </Tab.Pane>
+                                    </Tab.Content>
+                                </Tab.Container>
+                            </div>
+                            } 
+                        </TrackVisibility>
+                    </Col>
                 </Row>
-            </Container>      
+            </Container>
+            <img className="background-image-right" src={colorSharp2} alt="" />     
         </section>
     )
 }
