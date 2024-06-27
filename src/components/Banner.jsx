8deb008cd4b27f350fc9c1013250a296349f80fa
Anubhav Shail <anubhavshail12@gmail.com>
2024-06-27 23:44:21 +0530

    home page changed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -58,28 +58,14 @@ export const Banner = () => {
     }
 
     return (
-        <section className='banner' id='home'>
-            <Container className='mx-auto max-w-7xl px-4 sm:px-6 md:mt-0 lg:px-8 flex flex-col md:flex-row items-center justify-center md:justify-between h-screen'>
-                <Row className='align-items-center'>
-                    <Col xs={12} md={6} xl={7}>
-                        <TrackVisibility>
-                            <div className='banner__content'>
-                                <h1>{`Hi! I'm Anubhav Shail`} <span className='wrap'>{text}</span></h1>
-                                <p>
-                                    Looking to start the career as an entry-level software engineer with a reputed firm driven by technology. Proficient in C++, Python, GIT. Skilled in Web Development. Passionate about implementing and launching new projects.
-                                </p>
-                                <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/></button>
-                            </div>
-                        </TrackVisibility> 
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <div>
-                            {renderBackgroundImage()}
-                        </div> 
-                    </Col>
-                </Row>
-                
-            </Container>           
-        </section>
+        <>
+        <div>
+            <main className='mx-auto max-w-7xl px-4 sm:px-6 md:mt-0 lg:px-8 flex flex-col md:flex-row items-center justify-center md:justify-between h-screen'>
+                <div className='sm:text-center lg:text-left'>
+                    
+                </div>
+            </main>
+        </div>
+        </>
     )
 }
